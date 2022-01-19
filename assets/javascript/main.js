3c159a3d2fd158e9a0050d64cb32024658610a18
Osnago-L <osna.brera@gmail.com>
2022-01-19 11:31:49 +0100

    removed image message / added dropdown delete mess

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -16,17 +16,20 @@ var app = new Vue({
                     {
                     date: '10/01/2020 15:30:55',
                     text: 'Hai portato a spasso il cane?',
-                    status: 'sent'
+                    status: 'sent',
+                    dropdownActive: false,
                     },
                     {
                     date: '10/01/2020 15:50:00',
                     text: 'Ricordati di dargli da mangiare',
-                    status: 'sent'
+                    status: 'sent',
+                    dropdownActive: false,
                     },
                     {
                     date: '10/01/2020 16:15:22',
                     text: 'Tutto fatto!',
-                    status: 'received'
+                    status: 'received',
+                    dropdownActive: false,
                     }
                 ],
             },
@@ -38,17 +41,20 @@ var app = new Vue({
                     {
                     date: '20/03/2020 16:30:00',
                     text: 'Ciao come stai?',
-                    status: 'sent'
+                    status: 'sent',
+                    dropdownActive: false,
                     },
                     {
                     date: '20/03/2020 16:30:55',
                     text: 'Bene grazie! Stasera ci vediamo?',
-                    status: 'received'
+                    status: 'received',
+                    dropdownActive: false,
                     },
                     {
                     date: '20/03/2020 16:35:00',
                     text: 'Mi piacerebbe ma devo andare a fare la spesa.',
-                    status: 'sent'
+                    status: 'sent',
+                    dropdownActive: false,
                     }
                 ],
             },
@@ -60,17 +66,20 @@ var app = new Vue({
                     {
                     date: '28/03/2020 10:10:40',
                     text: 'La Marianna va in campagna',
-                    status: 'received'
+                    status: 'received',
+                    dropdownActive: false,
                     },
                     {
                     date: '28/03/2020 10:20:10',
                     text: 'Sicuro di non aver sbagliato chat?',
-                    status: 'sent'
+                    status: 'sent',
+                    dropdownActive: false,
                     },
                     {
                     date: '28/03/2020 16:15:22',
                     text: 'Ah scusa!',
-                    status: 'received'
+                    status: 'received',
+                    dropdownActive: false,
                     }
                 ],
             },
@@ -82,12 +91,14 @@ var app = new Vue({
                     {
                     date: '10/01/2020 15:30:55',
                     text: 'Lo sai che ha aperto una nuova pizzeria?',
-                    status: 'sent'
+                    status: 'sent',
+                    dropdownActive: false,
                     },
                     {
                     date: '10/01/2020 15:50:00',
                     text: 'Si, ma preferirei andare al cinema',
-                    status: 'received'
+                    status: 'received',
+                    dropdownActive: false,
                     }
                 ],
             },
@@ -107,17 +118,23 @@ var app = new Vue({
                 {
                     date: newdate,
                     text: this.inputchat,
-                    status: 'sent'
+                    status: 'sent',
+                    dropdownActive: false,
                     }
             );
             setTimeout(() => {this.contacts[this.refchat].messages.push(
                 {
                     date: newdate,
                     text: "ok",
-                    status: 'received'
+                    status: 'received',
+                    dropdownActive: false,
                     }
             );}, 1000);
             this.inputchat = ""
+        },
+        removeMessage: function(FIndex){
+            console.log(FIndex);
+            this.contacts[this.refchat].messages.splice(FIndex, 1);
         },
         searchChat: function(){
             return this.contacts.filter((element) => element.name.toLowerCase().indexOf(this.searchinput)==0);
@@ -130,8 +147,10 @@ var app = new Vue({
             })
         },
         paperPlaneActive: function(){
-            
             this.inputchat != "" ? this.sendbuttonactive = true : this.sendbuttonactive = false;
+        },
+        dropDownshow: function(FIndex){
+            this.contacts[this.refchat].messages[FIndex].dropdownActive = ! this.contacts[this.refchat].messages[FIndex].dropdownActive
         },
     },
   });
